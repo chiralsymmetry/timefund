d5eb09f3d3c2d16da6c981a0553fc2ca243b446c
David Nyman <david.nyman@protonmail.com>
2023-04-05 19:32:05 +0200

    Add main viewmodel

---
+++ TimeFund/ViewModels/TimeFundViewModel.cs
@@ -0,0 +1,12 @@
+﻿using TimeFund.Models;
+
+namespace TimeFund.ViewModels;
+
+public class TimeFundViewModel
+{
+    public TimeSpan TimeFund { get; set; } = TimeSpan.Zero;
+    public IEnumerable<Activity> AllActivities { get; set; } = Enumerable.Empty<Activity>();
+    public IEnumerable<Activity> NonNegativeActivities => AllActivities.Where(a => a.Multiplier >= 0);
+    public IEnumerable<Activity> NegativeActivities => AllActivities.Where(a => a.Multiplier < 0);
+    public Activity CurrentActivity { get; set; } = Activity.ZERO_ACTIVITY;
+}
