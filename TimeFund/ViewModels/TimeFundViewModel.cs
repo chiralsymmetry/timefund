a74a0a24396ed1058feb65e303c993287a560948
David Nyman <david.nyman@protonmail.com>
2023-04-05 20:18:33 +0200

    Expand viewmodel

--- TimeFund/ViewModels/TimeFundViewModel.cs
+++ TimeFund/ViewModels/TimeFundViewModel.cs
@@ -1,12 +1,25 @@
-﻿using TimeFund.Models;
+﻿using System.Diagnostics;
+using TimeFund.DataAccess;
+using TimeFund.Models;
+using Activity = TimeFund.Models.Activity;
 
 namespace TimeFund.ViewModels;
 
 public class TimeFundViewModel
 {
-    public TimeSpan TimeFund { get; set; } = TimeSpan.Zero;
-    public IEnumerable<Activity> AllActivities { get; set; } = Enumerable.Empty<Activity>();
+    public TimeSpan TimeFund { get; set; } = new();
+    public IEnumerable<Activity> AllActivities { get; set; }
     public IEnumerable<Activity> NonNegativeActivities => AllActivities.Where(a => a.Multiplier >= 0);
     public IEnumerable<Activity> NegativeActivities => AllActivities.Where(a => a.Multiplier < 0);
     public Activity CurrentActivity { get; set; } = Activity.ZERO_ACTIVITY;
+    public string TimerFormat => $"{(int)TimeFund.TotalHours:D2}:{TimeFund.Minutes:D2}:{TimeFund.Seconds:D2}";
+
+    private readonly Stopwatch stopwatch = new();
+    private readonly IDataAccess dataAccess;
+
+    public TimeFundViewModel(IDataAccess dataAccess)
+    {
+        this.dataAccess = dataAccess;
+        AllActivities = dataAccess.GetAllActivitiesAsync().Result;
+    }
 }
