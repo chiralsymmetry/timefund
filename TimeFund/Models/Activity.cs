e41a26f8291f6661797e45bae58d3f51a21605b5
David Nyman <david.nyman@protonmail.com>
2023-04-05 18:29:29 +0200

    Add activity model

---
+++ TimeFund/Models/Activity.cs
@@ -0,0 +1,10 @@
+﻿namespace TimeFund.Models;
+
+public class Activity
+{
+    public int Id { get; set; }
+    public string Icon { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public double Multiplier { get; set; } = 0;
+}
