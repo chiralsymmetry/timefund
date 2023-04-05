25e0d0422b4008abe1b429f3ec2e793b7995cea3
David Nyman <david.nyman@protonmail.com>
2023-04-05 18:54:29 +0200

    Add data access interface

---
+++ TimeFund/DataAccess/IDataAccess.cs
@@ -0,0 +1,12 @@
+﻿using TimeFund.Models;
+
+namespace TimeFund.DataAccess;
+
+public interface IDataAccess
+{
+    Task<int> InsertAsync(Activity activity);
+    Task<Activity?> GetActivity(int id);
+    Task<int> UpdateAsync(Activity activity);
+    Task<int> DeleteAsync(Activity activity);
+    Task<IEnumerable<Activity>> GetAllActivitiesAsync();
+}
