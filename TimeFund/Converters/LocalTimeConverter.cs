079cd17902c11e9956395b1f57f4016000271f6b
David Nyman <david.nyman@protonmail.com>
2023-04-11 18:17:59 +0200

    Use local time

---
+++ TimeFund/Converters/LocalTimeConverter.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+
+namespace TimeFund.Converters;
+
+internal class LocalTimeConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is DateTime dateTime)
+        {
+            return dateTime.ToLocalTime();
+        }
+
+        return value?.ToString() ?? string.Empty;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
