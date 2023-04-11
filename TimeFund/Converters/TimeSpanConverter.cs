aab096284070adb03868a1f5b5fe883b0b413790
David Nyman <david.nyman@protonmail.com>
2023-04-11 13:14:21 +0200

    Humanize timespan

--- TimeFund/Converters/TimeSpanConverter.cs
+++ TimeFund/Converters/TimeSpanConverter.cs
@@ -1,18 +1,29 @@
-﻿using System.Globalization;
+﻿using Humanizer;
+using System.Globalization;
 
 namespace TimeFund.Converters;
 
 public class TimeSpanConverter : IValueConverter
 {
+    public static string ColonFormat(TimeSpan timeSpan)
+    {
+        int hours = (int)timeSpan.TotalHours;
+        int minutes = timeSpan.Minutes;
+        int seconds = timeSpan.Seconds;
+        var timeSep = CultureInfo.CurrentCulture.DateTimeFormat.TimeSeparator;
+        return string.Format("{0:D2}{3}{1:D2}{3}{2:D2}", hours, minutes, seconds, timeSep);
+    }
+
+    public static string HumanFormat(TimeSpan timeSpan)
+    {
+        return timeSpan.Humanize(3, maxUnit: Humanizer.Localisation.TimeUnit.Hour);
+    }
+
     public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
     {
         if (value is TimeSpan timeSpan)
         {
-            int hours = (int)timeSpan.TotalHours;
-            int minutes = timeSpan.Minutes;
-            int seconds = timeSpan.Seconds;
-
-            return string.Format("{0:D2}:{1:D2}:{2:D2}", hours, minutes, seconds);
+            return ColonFormat(timeSpan);
         }
 
         return value?.ToString() ?? string.Empty;
