36071566a8a9f632235edf1dd12e73675cf67fd2
David Nyman <david.nyman@protonmail.com>
2023-04-11 18:18:48 +0200

    Add history page filter

--- TimeFund/ViewModels/AllUsageLogsViewModel.cs
+++ TimeFund/ViewModels/AllUsageLogsViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.ObjectModel;
+﻿using CommunityToolkit.Mvvm.Input;
+using System.Collections.ObjectModel;
 using TimeFund.DataAccess;
 using TimeFund.Models;
 
@@ -27,15 +28,96 @@ public class AllUsageLogsViewModel : ObservableViewModel
         }
     }
 
+    private DateTime fromLocalDateTime;
+
+    private DateTime FromLocalDateTime
+    {
+        get => fromLocalDateTime;
+        set
+        {
+            if (fromLocalDateTime != value)
+            {
+                fromLocalDateTime = value;
+                OnPropertyChanged(nameof(FromLocalDateTime));
+                OnPropertyChanged(nameof(FromLocalDate));
+                OnPropertyChanged(nameof(FromLocalTime));
+            }
+        }
+    }
+
+    public DateTime FromLocalDate
+    {
+        get => fromLocalDateTime.Date;
+        set => FromLocalDateTime = new DateTime(value.Year, value.Month, value.Day, fromLocalDateTime.Hour, fromLocalDateTime.Minute, fromLocalDateTime.Second);
+    }
+
+    public TimeSpan FromLocalTime
+    {
+        get => fromLocalDateTime.TimeOfDay;
+        set => FromLocalDateTime = fromLocalDateTime.Date + value;
+    }
+
+    private DateTime toLocalDateTime;
+
+    private DateTime ToLocalDateTime
+    {
+        get => toLocalDateTime;
+        set
+        {
+            if (toLocalDateTime != value)
+            {
+                toLocalDateTime = value;
+                OnPropertyChanged(nameof(ToLocalDateTime));
+                OnPropertyChanged(nameof(ToLocalDate));
+                OnPropertyChanged(nameof(ToLocalTime));
+            }
+        }
+    }
+
+    public DateTime ToLocalDate
+    {
+        get => toLocalDateTime.Date;
+        set => ToLocalDateTime = new DateTime(value.Year, value.Month, value.Day, toLocalDateTime.Hour, toLocalDateTime.Minute, toLocalDateTime.Second);
+    }
+
+    public TimeSpan ToLocalTime
+    {
+        get => toLocalDateTime.TimeOfDay;
+        set => ToLocalDateTime = toLocalDateTime.Date + value;
+    }
+
+    public List<Activity> Activities { get; }
+    public Activity SelectedActivity { get; set; }
+
     public AllUsageLogsViewModel(IDataAccess dataAccess)
     {
         this.dataAccess = dataAccess;
         AllUsageLogs = new();
+        Activities = Task.Run(dataAccess.GetAllActivitiesAsync).Result.OrderBy(a => a.Multiplier).ToList();
+        Activities.Insert(0, Activity.ZERO_ACTIVITY);
+        SelectedActivity = Activity.ZERO_ACTIVITY;
+        Reset();
+    }
+
+    private void Reset()
+    {
+        SelectedActivity = Activity.ZERO_ACTIVITY;
+        OnPropertyChanged(nameof(SelectedActivity));
+        FromLocalDateTime = DateTime.Now.AddMonths(-1);
+        ToLocalDateTime = DateTime.Now;
     }
 
     public async Task LoadUsageLogs()
     {
-        var allStoredUsageLogs = await dataAccess.GetAllUsageLogsAsync();
+        IEnumerable<UsageLog> allStoredUsageLogs;
+        if (SelectedActivity == Activity.ZERO_ACTIVITY)
+        {
+            allStoredUsageLogs = await dataAccess.GetAllUsageLogsOverlappingIntervalAsync(FromLocalDateTime.ToUniversalTime(), ToLocalDateTime.ToUniversalTime());
+        }
+        else
+        {
+            allStoredUsageLogs = await dataAccess.GetAllUsageLogsOverlappingIntervalForActivityAsync(SelectedActivity, FromLocalDateTime.ToUniversalTime(), ToLocalDateTime.ToUniversalTime());
+        }
         AllUsageLogs.Clear();
         foreach (var storedUsageLog in allStoredUsageLogs)
         {
@@ -43,4 +125,9 @@ public class AllUsageLogsViewModel : ObservableViewModel
         }
         SelectedUsageLog = UsageLog.ZERO_USAGELOG;
     }
+    private RelayCommand? loadUsageLogsCommand;
+    public IRelayCommand LoadUsageLogsCommand => loadUsageLogsCommand ??= new RelayCommand(async () => await LoadUsageLogs());
+
+    private RelayCommand? resetFilterCommand;
+    public IRelayCommand ResetFilterCommand => resetFilterCommand ??= new RelayCommand(async () => { Reset(); await LoadUsageLogs(); });
 }
